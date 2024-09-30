ba4d278707851a0d89899f9354c5e07fa3671be9
Musasizi Kenneth <24485588+Musasizi@users.noreply.github.com>
2024-09-30 16:30:19 +0300

    Commit has been made

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from today
+          Learn React from today This is so nice
         </a>
       </header>
     </div>
