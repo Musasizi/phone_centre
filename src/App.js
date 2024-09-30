835544a41632ab5c64a2fafb83f203c147a74de7
Musasizi Kenneth <24485588+Musasizi@users.noreply.github.com>
2024-09-30 16:45:59 +0300

    exclamation

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from today This is so nice
+          Learn React from today This is so nice!
         </a>
       </header>
     </div>
